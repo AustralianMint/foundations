fd4a1e138ec34a2154aaa9c59130b1543ee4da6b
Thumas <thomas.frey@code.berlin>
2019-03-13 15:55:24 +0100

    changed IP in simple-server.py to local ip

--- simple-server.py
+++ simple-server.py
@@ -5,6 +5,8 @@ PORT = 65432
 
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("127.0.0.1", PORT), Handler) as httpd:
+#with 127.0.0.1 it didn't work
+## had to change to local ip 192.168.0.12
+with socketserver.TCPServer(("192.168.0.12", PORT), Handler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
