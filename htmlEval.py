fe14808c7d7fbf0c6ac10cf6bfafa188ac7f692f
Thomas <thomas.frey@code.berlin>
2019-03-14 00:06:37 +0100

    close, but yet so far

--- htmlEval.py
+++ htmlEval.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python
+import cgi
+
+formData = cgi.fieldStorage()
+fav_color = formData.getvalue(fav-color)
+print("""
+<html>
+    <body>
+        <p>This is some html i'd like to be printed</p>
+    </body>
+</html>
+""")
--- simple-server.py
+++ simple-server.py
@@ -5,6 +5,6 @@ PORT = 65432
 
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("127.0.0.1", PORT), Handler) as httpd:
+with socketserver.TCPServer(("192.168.0.171", PORT), Handler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
