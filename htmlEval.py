602995ef61a675e5f213b6c9f3a90b47f33212e0
Thomas <thomas.frey@code.berlin>
2019-03-14 20:57:32 +0100

    re-arranged html

--- htmlEval.py
+++ htmlEval.py
@@ -1,12 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 import cgi
+import cvs
 
 formData = cgi.fieldStorage()
 fav_color = formData.getvalue(fav-color)
+
 print("""
 <html>
-    <body>
-        <p>This is some html i'd like to be printed</p>
-    </body>
+<body>
+<p>This is some html i'd like to be printed</p>
+</body>
 </html>
 """)
