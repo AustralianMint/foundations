7039d845b106e2fb115e59a460ee76202d910659
Thomas <thomas.frey@code.berlin>
2019-03-17 16:47:00 +0100

    Solving problem without CGI & HTTP

---
+++ colorCheckerStandAlone/colorChecker.py
@@ -0,0 +1,20 @@
+##Runs through given CSV file and checks if color is includedself.
+import csv
+fav_color = str(input("Enter your fav color: "))
+
+#opening csv and renaming to 'csv_file'
+with open('colors.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+
+    #Looping throug entre CSV file
+    for row in csv_reader:
+        if line_count == 0:
+            print(", ".join(row))
+            line_count += 1
+        else:
+            print("NAME1: {}, NAME2: {}, NAME3: {}".format(row[0], row[1], row[3]))
+            line_count += 1
+
+    #End with totat lines.
+    print("{} lines printed.".format(line_count))
