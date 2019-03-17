11d20219f0b07cb186fc909201b1a83a7ea26a20
Thomas <thomas.frey@code.berlin>
2019-03-17 18:40:58 +0100

    Deleted some nonsense

--- colorCheckerStandAlone/colorChecker1.py
+++ colorCheckerStandAlone/colorChecker1.py
@@ -14,22 +14,3 @@ with open('colors.csv', mode="r") as csv_file:
             line_count += 1
         else:
             print("I love {} too!".format(row["realName"]))
-
-print("""
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-<meta charset="utf-8">
-<link rel="stylesheet" type="text/css" href="style.css">
-<title></title>
-</head>
-<body>
-<h1>This is the site!</h1>
-<p>You have retrieved the info successfully!</p>
-<form action="scripts/htmlEval.py" method="get">
-<input type="text" name="fav-color">
-<input type="submit" value="SUBMIT">
-</form>
-</body>
-</html>
-""")
