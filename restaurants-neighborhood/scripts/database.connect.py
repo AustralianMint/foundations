906e1580d6dc9d0b79234c4c17042e1c227aaaa3
Thomas <thomas.frey@code.berlin>
2019-03-25 14:40:28 +0100

    Added func to print html/ added if statement to check database

--- restaurants-neighborhood/scripts/database.connect.py
+++ restaurants-neighborhood/scripts/database.connect.py
@@ -11,8 +11,8 @@ import cgi
 form = cgi.FieldStorage()
 
 # get an input filed from the form called 'fav_restaurant'
-# and assign it's value to a local variable called fav_restaurant_name
-fav_restaurant_name = form.getvalue('neighborhood')
+# and assign it's value to a local variable called selected_neighborhood
+selected_neighborhood = form.getvalue('neighborhood')
 
 # connect to the database file, and create a connection object
 db_connection = sqlite3.connect('restaurants.db')
@@ -21,18 +21,31 @@ db_connection = sqlite3.connect('restaurants.db')
 db_cursor = db_connection.cursor()
 
 #run a query depending on which button in the html form was selected. 
-if fav_restaurant_name == 'Kreuzberg':
+if selected_neighborhood == 'Kreuzberg':
   db_cursor.execute("SELECT NAME FROM restaurants WHERE NEIGHBORHOOD_ID = 1;")
-elif fav_restaurant_name == "Wedding":
+elif selected_neighborhood == "Wedding":
   db_cursor.execute("SELECT NAME FROM restaurants WHERE NEIGHBORHOOD_ID = 2;")
-elif fav_restaurant_name == 'Neuköln':
+elif selected_neighborhood == 'Neuköln':
   db_cursor.execute("SELECT NAME FROM restaurants WHERE NEIGHBORHOOD_ID = 3")
-else:
+elif selected_neighborhood == 'Spandau':
   db_cursor.execute("SELECT NAME FROM restaurants WHERE NEIGHBORHOOD_ID = 4")
+else:
+  print("No field was selected")
+  
 
 # store the result in a local variable.
 # this will be a list of tuples, where each tuple represents a row in the table
 list_restaurants = db_cursor.fetchall()
+
+def showRestaurants():
+  print("<table border='1'")
+  print("<tr>")
+  print("<th>Restaurant name</th>")
+  print("</tr>")
+  for each in list_restaurants:
+    print("<tr>")
+    print("<td>{}</td>".format(each))
+    print("</tr>")
 
 print("""
 <html>
@@ -41,14 +54,12 @@ print("""
     <title></title>
   </head>
   <body>
-    <h1>Here are the Re$taurants</h1>
-    <p>Here are the Restaurants in '{}'</p>
-    {} 
+    <h1 style="color:orange;">Here are the Restaurants</h1>
+    <p>Restaurants in '{}'</p> 
   </body>
 </html>
-""".format(fav_restaurant_name, list_restaurants))
+""".format(selected_neighborhood))
 
-# print("list_restaurants contents:")
-# print(list_restaurants)
+showRestaurants()
 
 db_connection.close()
