a5e2196865a8025ac098b064619afb1e3a52c107
Thomas <thomas.frey@code.berlin>
2019-03-28 17:44:07 +0100

    working random Dog retriever(html/py)

--- API/ApiPicture1.py
+++ API/ApiPicture1.py
@@ -1,23 +1,27 @@
+##cURL the dog api to provide a random picture.
+##turn json response into python dict.
+#access linked img in dict
+
 import requests
 import json
 
 r_with_data = requests.get('https://dog.ceo/api/breeds/image/random')
 converted_json_data = json.loads(r_with_data.text)
 
-print("""
+#new variable with html 
+#referencing json data -> .format it into <img src="{}"
+html = ("""
+<!DOCTYPE html>
 <html>
-    <style>
-        p {
-        color: red;
-        }
-    </style>
-    <body>
-        <p>
-        Thanks! %s is indeed a color
-        </p>
-    </body>
+<body>
+<p>Here is a picture of a dog, fak...</p>
+<img src="{}">
+</body>
 </html>
-""")
+""".format(converted_json_data['message']))
 
-print(converted_json_data['message'])
-print("This is the retrieved json data:\n{} ".format(converted_json_data))
+with open('dogHtmlFile.html', 'w') as file:
+    file.write(html)
+
+# print(converted_json_data['message'])
+# print("This is the retrieved json data:\n{} ".format(converted_json_data))
